3c8853780d1c8731f38038fe2f0f358cb70fd381
VengefulNick <89831583+VengefulNick@users.noreply.github.com>
2021-12-29 09:23:07 +0100

    flyttat runt kod

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -7,6 +7,7 @@ namespace Projekt
         public void Main()
         {
             bool showMenu = true;
+
             while (showMenu)
             {
                 MainMenu Menu = new MainMenu();
@@ -15,9 +16,37 @@ namespace Projekt
                 //showMenu = MainMenu.Meny();  
             }
 
-            // Klasser
-            // MainMenu     Ohmslaw      LengthConvert   SDTCalc   AreaVolume
         }
      }
 }
 
+
+
+//switch (Console.ReadLine())
+//{
+//    case "1":
+//        Console.WriteLine("1");
+//        break;
+//    case "2":
+//        Console.WriteLine("2");
+//        break;
+//    case "3":
+//        Console.WriteLine("3");
+//        break;
+//    case "4":
+//        Console.WriteLine("4");
+//        break;
+//    case "5":
+//        Console.WriteLine("5");
+//        break;
+//    default:
+//        Console.WriteLine("Wrong input try again!");
+//        Console.ReadLine();
+//        break;
+//}
+
+
+
+
+// Klasser
+// MainMenu     Ohmslaw      LengthConvert   SDTCalc   AreaVolume
