41bbfc1afff9a6e3cd43aeb5f11a50846ddb08e1
VengefulNick <89831583+VengefulNick@users.noreply.github.com>
2021-12-30 07:33:28 +0100

    fixed text, added comments

--- Projekt/MainMenu.cs
+++ Projekt/MainMenu.cs
@@ -11,19 +11,17 @@ namespace Projekt
         public void Meny()
         {
             Console.Clear();
-            Console.WriteLine("Hello and whalecum to Nicole, Daniela (they/them) and Samoyeh(tik/hona/catwoman) Project program! You can convert and messure your cock size here.");
+            Console.WriteLine("Welcome to Niklas, Daniel and Samiies Project!");
             Console.WriteLine();
-            Console.WriteLine("Choose one of the options below please. ryE?");
+            Console.WriteLine("» Main Menu «");
             Console.WriteLine();
-            Console.WriteLine("1. Ohmslaw. ryE?");
-            Console.WriteLine("2. Length converter. ryE?");
-            Console.WriteLine("3. Speed Distance Time Calculator. ryE?");
-            Console.WriteLine("4. Calculate Area/Volume. ryE?");
-            Console.WriteLine("5. Exit / breKK ryE?");
+            Console.WriteLine("1. Ohmslaw.");
+            Console.WriteLine("2. Length converter.");
+            Console.WriteLine("3. Speed Distance Time Calculator.");
+            Console.WriteLine("4. Calculate Area/Volume.");
+            Console.WriteLine("5. Exit / breKK");
             Console.WriteLine();
-            Console.WriteLine("Pick an option from the menu: ryE?");
-            Console.WriteLine("Please pick breKK since Daniel is supsugen!");
-            Console.WriteLine(" the booz is stored in the STACK! heEAIPP!");
+            Console.WriteLine("Pick an option from the menu:");
         }
 
 
--- Projekt/Ohmslaw.cs
+++ Projekt/Ohmslaw.cs
@@ -18,40 +18,40 @@ namespace Projekt
             {
 
                 Console.Clear();
-                Console.WriteLine("You have chosen Ohmslaw! ryE?");
+                Console.WriteLine("You have chosen Ohmslaw!");
                 Console.WriteLine();
-                Console.WriteLine("Which unit do you need to calculate? ryE?");
+                Console.WriteLine("Which unit do you need to calculate?");
                 Console.WriteLine();
-                Console.WriteLine("1. Voltage ryE?");
-                Console.WriteLine("2. Current ryE?");
-                Console.WriteLine("3. viva la Resistance ryE?");
-                Console.WriteLine("4. Return to mainmenu ryE?");
+                Console.WriteLine("1. Voltage");
+                Console.WriteLine("2. Current");
+                Console.WriteLine("3. viva la Resistance");
+                Console.WriteLine("4. Return to mainmenu");
                 Console.WriteLine();
-                Console.WriteLine("Choose an option: ryE?");
+                Console.WriteLine("Choose an option:");
                 Console.WriteLine();
                 string menuSelect = Console.ReadLine();
 
 
                 switch (menuSelect)
                 {
-                    case "1":   // U = R * I
+                    case "1":                                                                // U = R * I
                         Console.Clear();
                         Console.WriteLine();
-                        Console.WriteLine("You choose voltage! ryE?");
+                        Console.WriteLine("You choose voltage!");
                         Console.WriteLine();
-                        Console.WriteLine("Type the resistance: ryE?");
+                        Console.WriteLine("Type the resistance:");
                         resistanceString = Console.ReadLine();
                         while (!float.TryParse(resistanceString, out resistance))            // Error handling for input.
                         {
-                            Console.WriteLine("Input is not valid, try again! ryE?");
+                            Console.WriteLine("Input is not valid, try again!");
                             resistanceString = Console.ReadLine();
                         }
 
-                        Console.WriteLine("Type the ampere: ryE?");
+                        Console.WriteLine("Type the ampere:");
                         currentString = Console.ReadLine();
                         while (!float.TryParse(currentString, out current))                 // Error handling for input.
                         {
-                            Console.WriteLine("Input is not valid, try again! ryE?");
+                            Console.WriteLine("Input is not valid, try again!");
                             currentString = Console.ReadLine();
                         }
 
@@ -60,7 +60,7 @@ namespace Projekt
                         Console.ReadLine();
                         break;
 
-                    case "2":   // I = U / R
+                    case "2":                                                               // I = U / R
                         Console.Clear();
                         Console.WriteLine();
                         Console.WriteLine("You choose current!");
@@ -88,7 +88,7 @@ namespace Projekt
                         Console.ReadLine();
                         break;
 
-                    case "3":   // R = U / I
+                    case "3":                                                               // R = U / I
                         Console.Clear();
                         Console.WriteLine();
                         Console.WriteLine("You choose resistance!");
@@ -117,7 +117,7 @@ namespace Projekt
                         Console.ReadLine();
                         break;
                     case "4":
-                        loop = false;
+                        loop = false;                                                       // Stops the current loop and returns user to main menu.
                         break;
                     default:
                         Console.WriteLine("Wrong option, please enter an number between 1 and 4");
